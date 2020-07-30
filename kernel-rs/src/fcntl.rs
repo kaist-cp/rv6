43db37489b7e10dfc99bd3fc15675c83ec4ac74b
kimjungwow <jw950310@kaist.ac.kr>
2020-07-30 03:00:49 +0000

    Change Flags in fcntl.rs to Flags_fcntl, use from_bits_truncate(), and erase redundant clone()

--- kernel-rs/src/fcntl.rs
+++ kernel-rs/src/fcntl.rs
@@ -1,16 +1,9 @@
 bitflags! {
-    pub struct Flags: i32 {
+    pub struct Flags_fcntl: i32 {
         const O_RDONLY = 0;
         const O_WRONLY = 0x1;
         const O_RDWR = 0x2;
         const O_CREATE = 0x200;
         const O_MODE = 0;
-    }
-}
-
-impl Flags {
-    pub fn setbits(&mut self, n: i32) -> &mut Flags {
-        self.bits = n;
-        self
     }
 }
