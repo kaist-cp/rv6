7d9742d22bcc8e4fda967df71f64464bef3002fe
coolofficials <coolofficials@gmail.com>
2020-08-02 07:43:19 +0000

    FIX:remove_unnecessary_type_castings_sysproc.rs_trap.rs_virtio_disk.rs

--- kernel-rs/src/sysproc.rs
+++ kernel-rs/src/sysproc.rs
@@ -4,63 +4,72 @@ use crate::{
     spinlock::{acquire, release},
     syscall::{argaddr, argint},
     trap::{ticks, tickslock},
+    utils::spin_loop,
 };
+
 pub unsafe fn sys_exit() -> u64 {
     let mut n: i32 = 0;
-    if argint(0 as i32, &mut n) < 0 as i32 {
-        return -(1 as i32) as u64;
+    if argint(0, &mut n) < 0 {
+        return 1 as _;
     }
     exit(n);
-    0 as i32 as u64
+
     // not reached
+    spin_loop()
 }
+
 pub unsafe fn sys_getpid() -> u64 {
-    (*myproc()).pid as u64
+    (*myproc()).pid as _
 }
+
 pub unsafe fn sys_fork() -> u64 {
-    fork() as u64
+    fork() as _
 }
+
 pub unsafe fn sys_wait() -> u64 {
     let mut p: u64 = 0;
-    if argaddr(0 as i32, &mut p) < 0 as i32 {
-        return -(1 as i32) as u64;
+    if argaddr(0, &mut p) < 0 {
+        return -1 as _;
     }
-    wait(p) as u64
+    wait(p) as _
 }
+
 pub unsafe fn sys_sbrk() -> u64 {
     let mut addr: i32 = 0;
     let mut n: i32 = 0;
-    if argint(0 as i32, &mut n) < 0 as i32 {
-        return -(1 as i32) as u64;
+    if argint(0, &mut n) < 0 {
+        return -1 as _;
     }
     addr = (*myproc()).sz as i32;
-    if growproc(n) < 0 as i32 {
-        return -(1 as i32) as u64;
+    if growproc(n) < 0 {
+        return -1 as _;
     }
     addr as u64
 }
+
 pub unsafe fn sys_sleep() -> u64 {
     let mut n: i32 = 0;
     let mut ticks0: u32 = 0;
-    if argint(0 as i32, &mut n) < 0 as i32 {
-        return -(1 as i32) as u64;
+    if argint(0, &mut n) < 0 {
+        return -1 as _;
     }
     acquire(&mut tickslock);
     ticks0 = ticks;
     while ticks.wrapping_sub(ticks0) < n as u32 {
         if (*myproc()).killed != 0 {
             release(&mut tickslock);
-            return -(1 as i32) as u64;
+            return -1 as _;
         }
         sleep(&mut ticks as *mut u32 as *mut libc::c_void, &mut tickslock);
     }
     release(&mut tickslock);
-    0 as i32 as u64
+    0
 }
+
 pub unsafe fn sys_kill() -> u64 {
     let mut pid: i32 = 0;
-    if argint(0 as i32, &mut pid) < 0 as i32 {
-        return -(1 as i32) as u64;
+    if argint(0, &mut pid) < 0 {
+        return -1 as _;
     }
     kill(pid) as u64
 }
