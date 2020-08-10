d74fb9aec7c25e113afc72ac27cafa423eb29993
kimjungwow <jw950310@kaist.ac.kr>
2020-08-10 07:06:23 +0000

    Remove current_block and match in exec()

--- kernel-rs/src/exec.rs
+++ kernel-rs/src/exec.rs
@@ -15,7 +15,6 @@ use core::ptr;
 
 pub unsafe fn exec(mut path: *mut libc::c_char, mut argv: *mut *mut libc::c_char) -> i32 {
     let mut oldsz: usize = 0;
-    let mut current_block: usize;
     let mut s: *mut libc::c_char = ptr::null_mut();
     let mut last: *mut libc::c_char = ptr::null_mut();
     let mut argc: usize = 0;
@@ -29,6 +28,7 @@ pub unsafe fn exec(mut path: *mut libc::c_char, mut argv: *mut *mut libc::c_char
     let mut pagetable: pagetable_t = 0 as pagetable_t;
     let mut oldpagetable: pagetable_t = ptr::null_mut();
     let mut p: *mut proc = myproc();
+
     begin_op();
     ip = namei(path);
     if ip.is_null() {
@@ -36,6 +36,16 @@ pub unsafe fn exec(mut path: *mut libc::c_char, mut argv: *mut *mut libc::c_char
         return -1;
     }
     (*ip).lock();
+
+    let _op = scopeguard::guard((pagetable, sz, ip), |(pagetable, sz, ip)| {
+        if !pagetable.is_null() {
+            proc_freepagetable(pagetable, sz);
+        }
+        if !ip.is_null() {
+            (*ip).unlockput();
+            end_op();
+        }
+    });
 
     // Check ELF header
     if (*ip).read(
@@ -51,7 +61,6 @@ pub unsafe fn exec(mut path: *mut libc::c_char, mut argv: *mut *mut libc::c_char
         if !pagetable.is_null() {
             // Load program into memory.
             sz = 0;
-            current_block = 15768484401365413375;
             for i in 0..elf.phnum as usize {
                 let off = elf
                     .phoff
@@ -65,152 +74,137 @@ pub unsafe fn exec(mut path: *mut libc::c_char, mut argv: *mut *mut libc::c_char
                 ) as usize
                     != ::core::mem::size_of::<ProgHdr>()
                 {
-                    current_block = 7080392026674647309;
-                    break;
+                    return -1;
                 }
                 if ph.typ == ELF_PROG_LOAD as u32 {
                     if ph.memsz < ph.filesz {
-                        current_block = 7080392026674647309;
-                        break;
+                        return -1;
                     }
                     if ph.vaddr.wrapping_add(ph.memsz) < ph.vaddr {
-                        current_block = 7080392026674647309;
-                        break;
+                        return -1;
                     }
                     sz = uvmalloc(pagetable, sz, ph.vaddr.wrapping_add(ph.memsz));
                     if sz == 0 {
-                        current_block = 7080392026674647309;
-                        break;
+                        return -1;
                     }
                     if ph.vaddr.wrapping_rem(PGSIZE as usize) != 0 {
-                        current_block = 7080392026674647309;
-                        break;
+                        return -1;
                     }
                     if loadseg(pagetable, ph.vaddr, ip, ph.off as u32, ph.filesz as u32).is_err() {
-                        current_block = 7080392026674647309;
-                        break;
+                        return -1;
                     }
                 }
             }
+            (*ip).unlockput();
+            core::mem::forget(_op);
+            end_op();
+            ip = ptr::null_mut();
 
-            match current_block {
-                7080392026674647309 => {}
-                _ => {
+            p = myproc();
+            oldsz = (*p).sz;
+
+            // Allocate two pages at the next page boundary.
+            // Use the second as the user stack.
+            sz = sz.wrapping_add(PGSIZE as usize).wrapping_sub(1) & !(PGSIZE - 1) as usize;
+            sz = uvmalloc(pagetable, sz, sz.wrapping_add((2 * PGSIZE) as usize));
+            let _op = scopeguard::guard((pagetable, sz, ip), |(pagetable, sz, ip)| {
+                if !pagetable.is_null() {
+                    proc_freepagetable(pagetable, sz);
+                }
+                if !ip.is_null() {
                     (*ip).unlockput();
                     end_op();
-                    ip = ptr::null_mut();
-                    p = myproc();
-                    oldsz = (*p).sz;
+                }
+            });
+            if sz != 0 {
+                uvmclear(pagetable, sz.wrapping_sub((2 * PGSIZE) as usize));
+                sp = sz;
+                stackbase = sp.wrapping_sub(PGSIZE as usize);
 
-                    // Allocate two pages at the next page boundary.
-                    // Use the second as the user stack.
-                    sz = sz.wrapping_add(PGSIZE as usize).wrapping_sub(1) & !(PGSIZE - 1) as usize;
-                    sz = uvmalloc(pagetable, sz, sz.wrapping_add((2 * PGSIZE) as usize));
-                    if sz != 0 {
-                        uvmclear(pagetable, sz.wrapping_sub((2 * PGSIZE) as usize));
-                        sp = sz;
-                        stackbase = sp.wrapping_sub(PGSIZE as usize);
-
-                        // Push argument strings, prepare rest of stack in ustack.
-                        argc = 0;
-                        loop {
-                            if (*argv.add(argc)).is_null() {
-                                current_block = 4567019141635105728;
-                                break;
-                            }
-                            if argc >= MAXARG {
-                                current_block = 7080392026674647309;
-                                break;
-                            }
-                            sp = sp.wrapping_sub((strlen(*argv.add(argc)) + 1) as usize);
-
-                            // riscv sp must be 16-byte aligned
-                            sp = sp.wrapping_sub(sp.wrapping_rem(16));
-                            if sp < stackbase {
-                                current_block = 7080392026674647309;
-                                break;
-                            }
-                            if copyout(
-                                pagetable,
-                                sp,
-                                *argv.add(argc),
-                                (strlen(*argv.add(argc)) + 1) as usize,
-                            ) < 0
-                            {
-                                current_block = 7080392026674647309;
-                                break;
-                            }
-                            ustack[argc] = sp;
-                            argc = argc.wrapping_add(1)
-                        }
-                        match current_block {
-                            7080392026674647309 => {}
-                            _ => {
-                                ustack[argc] = 0;
-
-                                // push the array of argv[] pointers.
-                                sp = sp.wrapping_sub(
-                                    argc.wrapping_add(1)
-                                        .wrapping_mul(::core::mem::size_of::<usize>()),
-                                );
-                                sp = sp.wrapping_sub(sp.wrapping_rem(16));
-                                if sp >= stackbase
-                                    && copyout(
-                                        pagetable,
-                                        sp,
-                                        ustack.as_mut_ptr() as *mut libc::c_char,
-                                        argc.wrapping_add(1)
-                                            .wrapping_mul(::core::mem::size_of::<usize>()),
-                                    ) >= 0
-                                {
-                                    // arguments to user main(argc, argv)
-                                    // argc is returned via the system call return
-                                    // value, which goes in a0.
-                                    (*(*p).tf).a1 = sp;
-
-                                    // Save program name for debugging.
-                                    s = path;
-                                    last = s;
-                                    while *s != 0 {
-                                        if *s as i32 == '/' as i32 {
-                                            last = s.offset(1isize)
-                                        }
-                                        s = s.offset(1)
-                                    }
-                                    safestrcpy(
-                                        (*p).name.as_mut_ptr(),
-                                        last,
-                                        ::core::mem::size_of::<[libc::c_char; 16]>() as i32,
-                                    );
-
-                                    // Commit to the user image.
-                                    oldpagetable = (*p).pagetable;
-                                    (*p).pagetable = pagetable;
-                                    (*p).sz = sz;
-
-                                    // initial program counter = main
-                                    (*(*p).tf).epc = elf.entry;
-
-                                    // initial stack pointer
-                                    (*(*p).tf).sp = sp;
-                                    proc_freepagetable(oldpagetable, oldsz);
-
-                                    // this ends up in a0, the first argument to main(argc, argv)
-                                    return argc as i32;
-                                }
-                            }
-                        }
+                // Push argument strings, prepare rest of stack in ustack.
+                argc = 0;
+                loop {
+                    if (*argv.add(argc)).is_null() {
+                        break;
                     }
+                    if argc >= MAXARG {
+                        return -1;
+                    }
+                    sp = sp.wrapping_sub((strlen(*argv.add(argc)) + 1) as usize);
+
+                    // riscv sp must be 16-byte aligned
+                    sp = sp.wrapping_sub(sp.wrapping_rem(16));
+                    if sp < stackbase {
+                        return -1;
+                    }
+                    if copyout(
+                        pagetable,
+                        sp,
+                        *argv.add(argc),
+                        (strlen(*argv.add(argc)) + 1) as usize,
+                    ) < 0
+                    {
+                        return -1;
+                    }
+                    ustack[argc] = sp;
+                    argc = argc.wrapping_add(1)
+                }
+                ustack[argc] = 0;
+
+                // push the array of argv[] pointers.
+                sp = sp.wrapping_sub(
+                    argc.wrapping_add(1)
+                        .wrapping_mul(::core::mem::size_of::<usize>()),
+                );
+                sp = sp.wrapping_sub(sp.wrapping_rem(16));
+
+                if sp >= stackbase
+                    && copyout(
+                        pagetable,
+                        sp,
+                        ustack.as_mut_ptr() as *mut libc::c_char,
+                        argc.wrapping_add(1)
+                            .wrapping_mul(::core::mem::size_of::<usize>()),
+                    ) >= 0
+                {
+                    core::mem::forget(_op);
+                    // arguments to user main(argc, argv)
+                    // argc is returned via the system call return
+                    // value, which goes in a0.
+                    (*(*p).tf).a1 = sp;
+
+                    // Save program name for debugging.
+                    s = path;
+                    last = s;
+                    while *s != 0 {
+                        if *s as i32 == '/' as i32 {
+                            last = s.offset(1isize)
+                        }
+                        s = s.offset(1)
+                    }
+                    safestrcpy(
+                        (*p).name.as_mut_ptr(),
+                        last,
+                        ::core::mem::size_of::<[libc::c_char; 16]>() as i32,
+                    );
+
+                    // Commit to the user image.
+                    oldpagetable = (*p).pagetable;
+                    (*p).pagetable = pagetable;
+                    (*p).sz = sz;
+
+                    // initial program counter = main
+                    (*(*p).tf).epc = elf.entry;
+
+                    // initial stack pointer
+                    (*(*p).tf).sp = sp;
+                    proc_freepagetable(oldpagetable, oldsz);
+
+                    // this ends up in a0, the first argument to main(argc, argv)
+                    return argc as i32;
                 }
             }
         }
-    }
-    if !pagetable.is_null() {
-        proc_freepagetable(pagetable, sz);
-    }
-    if !ip.is_null() {
-        (*ip).unlockput();
-        end_op();
     }
     -1
 }
