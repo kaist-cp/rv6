a17cb2c7bd86aab2ef57885a244d5f8858195b7c
coolofficials <coolofficials@gmail.com>
2020-08-10 06:09:36 +0000

    cargo fmt

--- kernel-rs/src/kalloc.rs
+++ kernel-rs/src/kalloc.rs
@@ -2,12 +2,7 @@
 //! kernel stacks, page-table pages,
 //! and pipe buffers. Allocates whole 4096-byte pages.
 use crate::libc;
-use crate::{
-    memlayout::PHYSTOP,
-    printf::panic,
-    riscv::PGSIZE,
-    spinlock::Spinlock,
-};
+use crate::{memlayout::PHYSTOP, printf::panic, riscv::PGSIZE, spinlock::Spinlock};
 use core::ptr;
 
 /// first address after kernel.
@@ -49,8 +44,8 @@ pub unsafe fn kinit() {
 
 pub unsafe fn freerange(mut pa_start: *mut libc::c_void, mut pa_end: *mut libc::c_void) {
     let mut p = ((pa_start as usize)
-        .wrapping_add(PGSIZE as usize)	
-        .wrapping_sub(1)	
+        .wrapping_add(PGSIZE as usize)
+        .wrapping_sub(1)
         & !(PGSIZE - 1) as usize) as *mut libc::c_char;
     while p.offset(PGSIZE as isize) <= pa_end as *mut libc::c_char {
         kfree(p as *mut libc::c_void);
