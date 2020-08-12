b1bea734a5a099f7570d03c08408fb8e22430a32
coolofficials <coolofficials@gmail.com>
2020-08-12 03:33:07 +0000

    Change:FSMAGIC i32 to u32

--- kernel-rs/src/fs.rs
+++ kernel-rs/src/fs.rs
@@ -499,7 +499,7 @@ pub const ROOTINO: u32 = 1;
 
 /// block size
 pub const BSIZE: usize = 1024;
-pub const FSMAGIC: i32 = 0x10203040;
+pub const FSMAGIC: u32 = 0x10203040;
 pub const NDIRECT: usize = 12;
 
 pub const NINDIRECT: usize = BSIZE.wrapping_div(mem::size_of::<u32>());
