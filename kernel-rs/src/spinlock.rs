f3bbb3966c888d450498af32f011195ac247a2d4
anemoneflower <sallylucky@naver.com>
2020-08-13 01:32:44 +0000

    fix newlock

--- kernel-rs/src/spinlock.rs
+++ kernel-rs/src/spinlock.rs
@@ -119,10 +119,10 @@ pub struct Spinlock<T> {
 
 impl<T> Spinlock<T> {
     pub fn new(name: *mut u8, data: T) -> Self {
-        let mut newlock = RawSpinlock::zeroed();
-        newlock.initlock(name);
+        let mut lock = RawSpinlock::zeroed();
+        lock.initlock(name);
         Self {
-            lock: newlock,
+            lock: lock,
             data: UnsafeCell::new(data),
         }
     }
@@ -191,3 +191,104 @@ pub unsafe fn pop_off() {
         intr_on();
     };
 }
+
+
+
+/// Mutual exclusion lock.
+pub struct OldSpinlock {
+    /// Is the lock held?
+    locked: AtomicBool,
+
+    /// For debugging:
+
+    /// Name of lock.
+    name: *mut u8,
+
+    /// The cpu holding the lock.
+    cpu: *mut Cpu,
+}
+
+impl OldSpinlock {
+    // TODO: transient measure
+    pub const fn zeroed() -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            name: ptr::null_mut(),
+            cpu: ptr::null_mut(),
+        }
+    }
+
+    /// Mutual exclusion spin locks.
+    pub fn initlock(&mut self, name: *mut u8) {
+        (*self).name = name;
+        (*self).locked = AtomicBool::new(false);
+        (*self).cpu = ptr::null_mut();
+    }
+
+    /// Acquire the lock.
+    /// Loops (spins) until the lock is acquired.
+    pub unsafe fn acquire(&mut self) {
+        // disable interrupts to avoid deadlock.
+        push_off();
+        if self.holding() != 0 {
+            panic(b"acquire\x00" as *const u8 as *mut u8);
+        }
+
+        // On RISC-V, sync_lock_test_and_set turns into an atomic swap:
+        //   a5 = 1
+        //   s1 = &self->locked
+        //   amoswap.w.aq a5, a5, (s1)
+        while (*self)
+            .locked
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            .is_err()
+        {}
+
+        // Tell the C compiler and the processor to not move loads or stores
+        // past this point, to ensure that the critical section's memory
+        // references happen after the lock is acquired.
+        //
+        // TODO(@jeehoon): it's unnecessary.
+        //
+        // ::core::intrinsics::atomic_fence();
+
+        // Record info about lock acquisition for holding() and debugging.
+        (*self).cpu = mycpu();
+    }
+
+    /// Release the lock.
+    pub unsafe fn release(&mut self) {
+        if self.holding() == 0 {
+            panic(b"release\x00" as *const u8 as *mut u8);
+        }
+        (*self).cpu = ptr::null_mut();
+
+        // Tell the C compiler and the CPU to not move loads or stores
+        // past this point, to ensure that all the stores in the critical
+        // section are visible to other CPUs before the lock is released.
+        // On RISC-V, this turns into a fence instruction.
+        //
+        // TODO(@jeehoon): it's unnecessary.
+        //
+        // ::core::intrinsics::atomic_fence();
+
+        // Release the lock, equivalent to lk->locked = 0.
+        // This code doesn't use a C assignment, since the C standard
+        // implies that an assignment might be implemented with
+        // multiple store instructions.
+        // On RISC-V, sync_lock_release turns into an atomic swap:
+        //   s1 = &lk->locked
+        //   amoswap.w zero, zero, (s1)
+        (*self).locked.store(false, Ordering::Release);
+        pop_off();
+    }
+
+    /// Check whether this cpu is holding the lock.
+    pub unsafe fn holding(&mut self) -> i32 {
+        push_off();
+        let r: i32 = ((*self).locked.load(Ordering::Acquire) && (*self).cpu == mycpu()) as i32;
+        pop_off();
+        r
+    }
+}
+
