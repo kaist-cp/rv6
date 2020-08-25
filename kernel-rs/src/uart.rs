8e87fcddc929e0417a81453f97768dab27fdb2c5
coolofficials <coolofficials@gmail.com>
2020-08-25 10:21:00 +0000

    encapsulate UartCtrlRegs

--- kernel-rs/src/uart.rs
+++ kernel-rs/src/uart.rs
@@ -3,84 +3,94 @@ use crate::console::consoleintr;
 use crate::memlayout::UART0;
 use core::ptr;
 
+use self::UartCtrlRegs::{FCR, IER, ISR, LCR, LSB, LSR, MSB, RHR, THR};
+
+/// The UART control registers.
+/// Some have different meanings for
+/// read vs write.
+/// http://byterunner.com/16550.html
+#[repr(usize)]
+enum UartCtrlRegs {
+    /// Receive Holding Register (for input bytes).
+    RHR,
+    /// Transmit Holding Register (for output bytes).
+    THR,
+    /// Interrupt Enable Register.
+    IER,
+    /// FIFO Control Register.
+    FCR,
+    /// Interrupt Status Register.
+    ISR,
+    /// Line Control Register.
+    LCR,
+    /// Line Status Register.
+    LSR,
+    /// LSB for baud rate.
+    LSB,
+    /// MSB for baud rate.
+    MSB,
+}
+
 /// the UART control registers are memory-mapped
 /// at address UART0. this macro returns the
 /// address of one of the registers.
-#[inline]
-const fn reg(r: usize) -> *mut u8 {
-    (UART0 + r) as *mut u8
+impl UartCtrlRegs {
+    unsafe fn reg(self) -> *mut u8 {
+        match self {
+            RHR | THR | LSB => (UART0 as *mut u8).add(0 as _),
+            IER | MSB => (UART0 as *mut u8).add(1 as _),
+            FCR | ISR => (UART0 as *mut u8).add(2 as _),
+            LCR => (UART0 as *mut u8).add(3 as _),
+            LSR => (UART0 as *mut u8).add(5 as _),
+        }
+    }
+
+    unsafe fn read(self) -> u8 {
+        ptr::read_volatile(self.reg())
+    }
+
+    unsafe fn write(self, v: u8) {
+        ptr::write_volatile(self.reg(), v)
+    }
 }
-
-unsafe fn read_reg(r: usize) -> u8 {
-    ptr::read_volatile(reg(r))
-}
-unsafe fn write_reg(r: usize, v: u8) {
-    ptr::write_volatile(reg(r), v)
-}
-
-/// the UART control registers.
-/// some have different meanings for
-/// read vs write.
-/// http://byterunner.com/16550.html
-
-/// receive holding register (for input bytes)
-const RHR: usize = 0;
-
-/// transmit holding register (for output bytes)
-const THR: usize = 0;
-
-/// interrupt enable register
-const IER: usize = 1;
-
-/// FIFO control register
-const FCR: usize = 2;
-
-/// interrupt status register
-const ISR: usize = 2;
-
-/// line control register
-const LCR: usize = 3;
-
-/// line status register
-const LSR: usize = 5;
 
 pub unsafe fn uartinit() {
     // disable interrupts.
-    write_reg(IER, 0x00);
+    IER.write(0x00);
 
     // special mode to set baud rate.
-    write_reg(LCR, 0x80);
+    LCR.write(0x80);
 
     // LSB for baud rate of 38.4K.
-    write_reg(0, 0x03);
+    LSB.write(0x03);
 
     // MSB for baud rate of 38.4K.
-    write_reg(1, 0x00);
+    MSB.write(0x00);
 
     // leave set-baud mode,
     // and set word length to 8 bits, no parity.
-    write_reg(LCR, 0x03);
+    LCR.write(0x03);
 
     // reset and enable FIFOs.
-    write_reg(FCR, 0x07);
+    FCR.write(0x07);
 
     // enable receive interrupts.
-    write_reg(IER, 0x01);
+    IER.write(0x01);
 }
 
 /// write one output character to the UART.
 pub unsafe fn uartputc(c: i32) {
     // wait for Transmit Holding Empty to be set in LSR.
-    while read_reg(LSR) & 1 << 5 == 0 {}
-    write_reg(THR, c as u8);
+    while LSR.read() & 1 << 5 == 0 {}
+    THR.write(c as u8);
 }
 
 /// read one input character from the UART.
 /// return -1 if none is waiting.
 pub unsafe fn uartgetc() -> i32 {
-    if read_reg(LSR) & 0x1 != 0 {
+    if LSR.read() & 0x1 != 0 {
         // input data is ready.
-        read_reg(RHR) as i32
+        RHR.read() as i32
     } else {
         -1
     }
