3a9ae812a36a13f3dc5eb6b399ecd49afdc76623
Sanguk Park <efenniht@furiosa.ai>
2020-09-13 03:59:06 +0900

    Don't inline spin_loop().

--- kernel-rs/src/utils.rs
+++ kernel-rs/src/utils.rs
@@ -1,5 +1,8 @@
 use core::sync::atomic::spin_loop_hint;
 
+// HACK(@efenniht): Block inlining to avoid an infinite loop miscompilation of LLVM:
+// https://github.com/rust-lang/rust/issues/28728.
+#[inline(never)]
 pub fn spin_loop() -> ! {
     loop {
         spin_loop_hint();
