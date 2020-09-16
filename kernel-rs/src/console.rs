2eeaf5f313c60790094b576417fde5eeb34a031e
kimjungwow <jw950310@kaist.ac.kr>
2020-09-16 11:42:16 +0000

    Rename CONSOLE=1 to CONSOLE_IN_DEVSW

--- kernel-rs/src/console.rs
+++ kernel-rs/src/console.rs
@@ -10,6 +10,7 @@ use crate::{
 use core::fmt;
 use core::sync::atomic::Ordering;
 
+const CONSOLE_IN_DEVSW: usize = 1;
 /// Size of console input buffer.
 const INPUT_BUF: usize = 128;
 
@@ -58,10 +59,9 @@ impl Console {
 
         // Connect read and write system calls
         // to consoleread and consolewrite.
-        let console = DEVSW.as_mut_ptr().add(1);
-        let fresh2 = &mut (*console).read;
+        let fresh2 = &mut (*DEVSW.as_mut_ptr().add(CONSOLE_IN_DEVSW)).read;
         *fresh2 = Some(consoleread as unsafe fn(_: i32, _: usize, _: i32) -> i32);
-        let fresh3 = &mut (*console).write;
+        let fresh3 = &mut (*DEVSW.as_mut_ptr().add(CONSOLE_IN_DEVSW)).write;
         *fresh3 = Some(consolewrite as unsafe fn(_: i32, _: usize, _: i32) -> i32);
     }
 
