0c17265b1330714f98d84478bfaeb05d0518d833
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2020-10-18 04:18:47 +0900

    Create kernel object

--- kernel-rs/src/kernel.rs
+++ kernel-rs/src/kernel.rs
@@ -1,24 +1,68 @@
+use core::mem;
+use core::sync::atomic::{spin_loop_hint, AtomicBool, Ordering};
+
 use crate::{
     bio::binit,
-    console::Console,
+    console::{consoleinit, Console},
     kalloc::kinit,
     plic::{plicinit, plicinithart},
-    printf::printfinit,
     println,
     proc::{cpuid, scheduler, PROCSYS},
+    sleepablelock::Sleepablelock,
     trap::{trapinit, trapinithart},
+    uart::Uart,
     virtio_disk::virtio_disk_init,
     vm::{kvminit, kvminithart},
 };
-use core::sync::atomic::{spin_loop_hint, AtomicBool, Ordering};
+
+/// The kernel.
+pub static mut KERNEL: mem::MaybeUninit<Kernel> = mem::MaybeUninit::uninit();
+
+/// The kernel can be mutably accessed only during the initialization.
+pub unsafe fn kernel_mut() -> &'static mut Kernel {
+    &mut *KERNEL.as_mut_ptr()
+}
+
+/// After intialized, the kernel is safe to immutably access.
+pub fn kernel() -> &'static Kernel {
+    unsafe { &*KERNEL.as_ptr() }
+}
+
+pub struct Kernel {
+    panicked: AtomicBool,
+
+    /// Sleeps waiting for there are some input in console buffer.
+    pub console: Sleepablelock<Console>,
+}
+
+impl Kernel {
+    fn panic(&self) {
+        self.panicked.store(true, Ordering::Release);
+    }
+
+    pub fn is_panicked(&self) -> bool {
+        self.panicked.load(Ordering::Acquire)
+    }
+}
+
+/// Handles panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic_handler(info: &core::panic::PanicInfo<'_>) -> ! {
+    // Freeze other CPUs.
+    kernel().panic();
+    println!("{}", info);
+
+    crate::utils::spin_loop()
+}
 
 static STARTED: AtomicBool = AtomicBool::new(false);
 
 /// start() jumps here in supervisor mode on all CPUs.
 pub unsafe fn kernel_main() {
     if cpuid() == 0 {
-        Console::init();
-        printfinit();
+        let uart = Uart::new();
+        kernel_mut().console = Sleepablelock::new("CONS", Console::new(uart));
 
         println!();
         println!("rv6 kernel is booting");
@@ -53,6 +97,8 @@ pub unsafe fn kernel_main() {
 
         // Emulated hard disk.
         virtio_disk_init();
+
+        consoleinit();
 
         // First user process.
         PROCSYS.user_proc_init();
--- kernel-rs/src/printf.rs
+++ kernel-rs/src/printf.rs
@@ -1,26 +1,22 @@
-//! formatted console output -- println, panic.
-use crate::console::CONS;
+//! formatted console output -- println.
+use crate::kernel::kernel;
 use core::fmt;
-use core::sync::atomic::{AtomicBool, Ordering};
-
-pub static PANICKED: AtomicBool = AtomicBool::new(false);
-pub static LOCKING: AtomicBool = AtomicBool::new(false);
-
-pub fn printfinit() {
-    LOCKING.store(true, Ordering::Release);
-}
 
 /// Prints the given formatted string with the Console.
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments<'_>) {
     use core::fmt::Write;
 
-    if LOCKING.load(Ordering::Relaxed) {
-        let mut lock = CONS.lock();
+    if !kernel().is_panicked() {
+        let mut lock = kernel().console.lock();
         lock.write_fmt(args).unwrap();
     } else {
         unsafe {
-            CONS.get_mut_unchecked().write_fmt(args).unwrap();
+            kernel()
+                .console
+                .get_mut_unchecked()
+                .write_fmt(args)
+                .unwrap();
         }
     }
 }
@@ -36,17 +32,4 @@ macro_rules! print {
 macro_rules! println {
     () => ($crate::print!("\n"));
     ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
-}
-
-/// Handles panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic_handler(info: &core::panic::PanicInfo<'_>) -> ! {
-    LOCKING.store(false, Ordering::Relaxed);
-    println!("{}", info);
-
-    // Freeze other CPUs.
-    PANICKED.store(true, Ordering::Release);
-
-    crate::utils::spin_loop()
 }
