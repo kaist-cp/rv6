e4dfdd5757b38975105536dd8260c8252c419e23
coolofficials <coolofficials@gmail.com>
2020-11-23 02:14:26 +0000

    Grammar fixed

--- kernel-rs/src/console.rs
+++ kernel-rs/src/console.rs
@@ -16,7 +16,7 @@ const CONSOLE_IN_DEVSW: usize = 1;
 const INPUT_BUF: usize = 128;
 
 /// The integrated interface for console device.
-/// Managing every communications between real-world users and system.
+/// Managing every communication between real-world users and system.
 pub struct Console {
     /// An interface for user programs.
     /// I/O interactions between users and processes are done by this interface.
