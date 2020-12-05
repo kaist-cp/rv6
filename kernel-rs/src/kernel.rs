0af7578debde7efab4e20083f49976f4ebcdbbdb
kimjungwow <jw950310@kaist.ac.kr>
2020-12-05 04:53:58 +0000

    FIX : Add TODO to remove `pub` from `KERNEL`

--- kernel-rs/src/kernel.rs
+++ kernel-rs/src/kernel.rs
@@ -25,6 +25,7 @@ use crate::{
 };
 
 /// The kernel.
+// TODO(rv6): remove pub from `pub static mut KERNEL`.
 pub static mut KERNEL: Kernel = Kernel::zero();
 
 /// After intialized, the kernel is safe to immutably access.
