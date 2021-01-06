69f408cdee1002f936db4980aee14f1f5ee96548
travis1829 <travis1829@naver.com>
2021-01-06 06:59:18 +0000

    Formatted comments in trap.rs (#154).

--- kernel-rs/src/trap.rs
+++ kernel-rs/src/trap.rs
@@ -19,19 +19,19 @@ extern "C" {
 
     static mut userret: [u8; 0];
 
-    // in kernelvec.S, calls kerneltrap().
+    // In kernelvec.S, calls kerneltrap().
     fn kernelvec();
 }
 
 pub unsafe fn trapinit() {}
 
-/// set up to take exceptions and traps while in the kernel.
+/// Set up to take exceptions and traps while in the kernel.
 pub unsafe fn trapinithart() {
     w_stvec(kernelvec as _);
 }
 
-/// handle an interrupt, exception, or system call from user space.
-/// called from trampoline.S
+/// Handle an interrupt, exception, or system call from user space.
+/// Called from trampoline.S.
 #[no_mangle]
 pub unsafe extern "C" fn usertrap() {
     let mut which_dev: i32 = 0;
@@ -41,14 +41,14 @@ pub unsafe extern "C" fn usertrap() {
         "usertrap: not from user mode"
     );
 
-    // send interrupts and exceptions to kerneltrap(),
+    // Send interrupts and exceptions to kerneltrap(),
     // since we're now in the kernel.
     w_stvec(kernelvec as _);
 
     let p: *mut Proc = myproc();
     let mut data = &mut *(*p).data.get();
 
-    // save user program counter.
+    // Save user program counter.
     (*data.trapframe).epc = r_sepc();
     if r_scause() == 8 {
         // system call
@@ -61,7 +61,7 @@ pub unsafe extern "C" fn usertrap() {
         // but we want to return to the next instruction.
         (*data.trapframe).epc = ((*data.trapframe).epc).wrapping_add(4);
 
-        // an interrupt will change sstatus &c registers,
+        // An interrupt will change sstatus &c registers,
         // so don't enable until done with those registers.
         intr_on();
         kernel().syscall();
@@ -86,7 +86,7 @@ pub unsafe extern "C" fn usertrap() {
         kernel().procs.exit_current(-1);
     }
 
-    // give up the CPU if this is a timer interrupt.
+    // Give up the CPU if this is a timer interrupt.
     if which_dev == 2 {
         proc_yield();
     }
@@ -94,22 +94,22 @@ pub unsafe extern "C" fn usertrap() {
     usertrapret();
 }
 
-/// return to user space
+/// Return to user space.
 pub unsafe fn usertrapret() {
     let p: *mut Proc = myproc();
     let mut data = &mut *(*p).data.get();
 
-    // we're about to switch the destination of traps from
+    // We're about to switch the destination of traps from
     // kerneltrap() to usertrap(), so turn off interrupts until
     // we're back in user space, where usertrap() is correct.
     intr_off();
 
-    // send syscalls, interrupts, and exceptions to trampoline.S
+    // Send syscalls, interrupts, and exceptions to trampoline.S.
     w_stvec(
         TRAMPOLINE.wrapping_add(uservec.as_mut_ptr().offset_from(trampoline.as_mut_ptr()) as usize),
     );
 
-    // set up trapframe values that uservec will need when
+    // Set up trapframe values that uservec will need when
     // the process next re-enters the kernel.
 
     // kernel page table
@@ -122,26 +122,26 @@ pub unsafe fn usertrapret() {
     // hartid for cpuid()
     (*data.trapframe).kernel_hartid = r_tp();
 
-    // set up the registers that trampoline.S's sret will use
+    // Set up the registers that trampoline.S's sret will use
     // to get to user space.
 
-    // set S Previous Privilege mode to User.
+    // Set S Previous Privilege mode to User.
     let mut x = Sstatus::read();
 
-    // clear SPP to 0 for user mode
+    // Clear SPP to 0 for user mode.
     x.remove(Sstatus::SPP);
 
-    // enable interrupts in user mode
+    // Enable interrupts in user mode.
     x.insert(Sstatus::SPIE);
     x.write();
 
-    // set S Exception Program Counter to the saved user pc.
+    // Set S Exception Program Counter to the saved user pc.
     w_sepc((*data.trapframe).epc);
 
-    // tell trampoline.S the user page table to switch to.
+    // Tell trampoline.S the user page table to switch to.
     let satp: usize = make_satp(data.pagetable.as_raw() as usize);
 
-    // jump to trampoline.S at the top of memory, which
+    // Jump to trampoline.S at the top of memory, which
     // switches to the user page table, restores user registers,
     // and switches to user mode with sret.
     let fn_0: usize =
@@ -150,7 +150,7 @@ pub unsafe fn usertrapret() {
     fn_0(TRAPFRAME, satp);
 }
 
-/// interrupts and exceptions from kernel code go here via kernelvec,
+/// Interrupts and exceptions from kernel code go here via kernelvec,
 /// on whatever the current kernel stack is.
 #[no_mangle]
 pub unsafe fn kerneltrap() {
@@ -175,12 +175,12 @@ pub unsafe fn kerneltrap() {
         panic!("kerneltrap");
     }
 
-    // give up the CPU if this is a timer interrupt.
+    // Give up the CPU if this is a timer interrupt.
     if which_dev == 2 && !myproc().is_null() && (*myproc()).state() == Procstate::RUNNING {
         proc_yield();
     }
 
-    // the yield() may have caused some traps to occur,
+    // The yield() may have caused some traps to occur,
     // so restore trap registers for use by kernelvec.S's sepc instruction.
     w_sepc(sepc);
     sstatus.write();
@@ -192,16 +192,16 @@ pub unsafe fn clockintr() {
     ticks.wakeup();
 }
 
-/// check if it's an external interrupt or software interrupt,
+/// Check if it's an external interrupt or software interrupt,
 /// and handle it.
-/// returns 2 if timer interrupt,
+/// Returns 2 if timer interrupt,
 /// 1 if other device,
 /// 0 if not recognized.
 pub unsafe fn devintr() -> i32 {
     let scause: usize = r_scause();
 
     if scause & 0x8000000000000000 != 0 && scause & 0xff == 9 {
-        // this is a supervisor external interrupt, via PLIC.
+        // This is a supervisor external interrupt, via PLIC.
 
         // irq indicates which device interrupted.
         let irq: usize = plic_claim();
@@ -214,7 +214,7 @@ pub unsafe fn devintr() -> i32 {
             println!("unexpected interrupt irq={:018p}\n", irq as *const u8);
         }
 
-        // the PLIC allows each device to raise at most one
+        // The PLIC allows each device to raise at most one
         // interrupt at a time; tell the PLIC the device is
         // now allowed to interrupt again.
         if irq != 0 {
@@ -223,14 +223,14 @@ pub unsafe fn devintr() -> i32 {
 
         1
     } else if scause == 0x8000000000000001 {
-        // software interrupt from a machine-mode timer interrupt,
+        // Software interrupt from a machine-mode timer interrupt,
         // forwarded by timervec in kernelvec.S.
 
         if cpuid() == 0 {
             clockintr();
         }
 
-        // acknowledge the software interrupt by clearing
+        // Acknowledge the software interrupt by clearing
         // the SSIP bit in sip.
         w_sip(r_sip() & !2);
 
