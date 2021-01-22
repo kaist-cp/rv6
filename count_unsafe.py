4de59b72880a3e7dfaf4ca28bb32cd138c9e8829
Sanguk Park <sanguk.park@kaist.ac.kr>
2021-01-22 15:51:44 +0900

    Fix bug: count source codes in subdirectories.

--- count_unsafe.py
+++ count_unsafe.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, re, sys, glob
+import subprocess, re, sys, glob
 from collections import defaultdict
 
 path = 'kernel-rs/**/*.rs'
@@ -8,12 +8,12 @@ path = 'kernel-rs/**/*.rs'
 if len(sys.argv) > 1:
     path = sys.argv[1]
 
-if os.system('which count-unsafe > /dev/null 2>&1') != 0:
+if subprocess.call('which count-unsafe > /dev/null 2>&1', shell=True) != 0:
     print('''Please install count-unsafe by\n
 `rustup update nightly && cargo +nightly install --git https://github.com/efenniht/count-unsafe`''')
     exit(-1)
 
-if os.system('which cloc > /dev/null 2>&1') != 0:
+if subprocess.call('which cloc > /dev/null 2>&1', shell=True) != 0:
     print('''Please install cloc by `apt install cloc`''')
     exit(-1)
 
@@ -21,21 +21,23 @@ space = re.compile(r'\s+')
 
 unsafes = defaultdict(lambda: 0)
 
-for line in os.popen(f'count-unsafe {path}').readlines()[1:]:
-    file, begin, end, cnt, ty = line.split(',')
-
-    unsafes[file] += int(cnt)
-
 slocs = {}
 
 for file in glob.glob(path, recursive=True):
-    stat = os.popen(f'cloc {file}').readlines()[-2].strip()
+    for line in subprocess.check_output(['count-unsafe', file], text=True).splitlines()[1:]:
+        file, begin, end, cnt, ty = line.split(',')
+        unsafes[file] += int(cnt)
+
+    stat = subprocess.check_output(['cloc', file], text=True).splitlines()[-2].strip()
     sloc = int(space.split(stat)[-1])
 
     slocs[file] = sloc
 
-    print(f'{file} : {unsafes[file]}/{sloc} = {unsafes[file]*100//sloc}')
+    print(f'{file}: {unsafes[file]}/{sloc} = {unsafes[file]*100//sloc}%')
 
 print('Total:')
 
-print(f'{sum(unsafes.values())}/{sum(slocs.values())}')
+unsafe_total = sum(unsafes.values())
+sloc_total = sum(slocs.values())
+
+print(f'{unsafe_total}/{sloc_total} = {unsafe_total*100//sloc_total}%')
