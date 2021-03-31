f9931c4d336674784630072dd5f244485de5cf38
Jaemin Hong <hjm0901@gmail.com>
2021-03-31 14:21:26 +0900

    Use `List` for `Kmem`

--- kernel-rs/src/page.rs
+++ kernel-rs/src/page.rs
@@ -1,11 +1,12 @@
 use core::{
     mem,
+    mem::MaybeUninit,
     ops::{Deref, DerefMut},
     ptr,
     ptr::NonNull,
 };
 
-use crate::{println, riscv::PGSIZE, vm::PAddr};
+use crate::{riscv::PGSIZE, vm::PAddr};
 
 /// Page type.
 #[repr(align(4096))]
@@ -71,6 +72,23 @@ impl Page {
             inner: unsafe { NonNull::new_unchecked(addr as *mut _) },
         }
     }
+
+    pub fn as_uninit_mut<T>(&mut self) -> &mut MaybeUninit<T> {
+        // TODO(https://github.com/kaist-cp/rv6/issues/471):
+        // Use const_assert! (or equivalent) instead. Currently, use of T inside const_assert!
+        // incurs a compile error: "can't use generic parameters from outer function".
+        // Also, there's a workaround using feature(const_generics) and
+        // feature(const_evaluatable_checked). However, using them makes the compiler panic.
+        // When the compiler becomes updated, we will fix the following lines to use static checks.
+        assert!(mem::size_of::<T>() <= PGSIZE);
+        assert_eq!(PGSIZE % mem::align_of::<T>(), 0);
+
+        // SAFETY: self.inner is an array of length PGSIZE aligned with PGSIZE bytes.
+        // The above assertions show that it can contain a value of T. As it contains arbitrary
+        // data, we cannot treat it as &mut T. Instead, we use &mut MaybeUninit<T>. It's ok because
+        // T and MaybeUninit<T> have the same size, alignment, and ABI.
+        unsafe { &mut *(self.inner.as_ptr() as *mut MaybeUninit<T>) }
+    }
 }
 
 impl Deref for Page {
@@ -91,7 +109,6 @@ impl Drop for Page {
     fn drop(&mut self) {
         // HACK(@efenniht): we really need linear type here:
         // https://github.com/rust-lang/rfcs/issues/814
-        println!("page must never drop.");
         panic!("Page must never drop.");
     }
 }
