309452c4986872ad2db2a0a06383081f89723e7e
travis1829 <travis1829@naver.com>
2021-04-07 07:56:38 +0000

    Add `BrandedLock`.

--- kernel-rs/src/lock/lock_protected.rs
+++ kernel-rs/src/lock/lock_protected.rs
@@ -1,8 +1,16 @@
-use core::{cell::UnsafeCell, pin::Pin, ptr};
+use core::{
+    cell::{Cell, UnsafeCell},
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+    pin::Pin,
+    ptr,
+};
 
 use super::{
-    sleepablelock::RawSleepablelock, sleeplock::RawSleeplock, spinlock::RawSpinlock, Guard, Lock,
-    RawLock,
+    sleepablelock::RawSleepablelock,
+    sleeplock::RawSleeplock,
+    spinlock::{RawSpinlock, Spinlock},
+    Guard, Lock, RawLock,
 };
 
 /// `RemoteLock<'s, R, U, T>`, such as `RemoteLock<'s, RawSpinlock, U, T>`.
@@ -90,4 +98,124 @@ impl<'s, R: RawLock, U, T: Unpin> RemoteLock<'s, R, U, T> {
     pub fn get_mut<'a: 'b, 'b>(&'a self, guard: &'b mut Guard<'_, R, U>) -> &'b mut T {
         self.get_pin_mut(guard).get_mut()
     }
+}
+
+/* Experimental */
+// TODO: Add more documentation
+
+/// A type that holds an invariant, unique lifetime called `'id`.
+/// This lifetime is actually more like an identifier/token, rather than a lifetime.
+type Id<'id> = PhantomData<Cell<&'id mut ()>>;
+
+/// A branded lock.
+/// The `'id` tag uniquely distinguishes `BrandedLock`s and its related `BrandedGuard`/`BrandedRemoteLock`.
+pub struct BrandedLock<'id, R: RawLock, T> {
+    _marker: Id<'id>,
+    lock: Lock<R, T>,
+}
+
+pub type BrandedSleepablelock<'id, T> = BrandedLock<'id, RawSleepablelock, T>;
+pub type BrandedSleeplock<'id, T> = BrandedLock<'id, RawSleeplock, T>;
+pub type BrandedSpinlock<'id, T> = BrandedLock<'id, RawSpinlock, T>;
+
+pub struct BrandedGuard<'id, 's, R: RawLock, T> {
+    _marker: Id<'id>,
+    guard: Guard<'s, R, T>,
+}
+
+pub type BrandedSleepablelockGuard<'id, 's, T> = BrandedGuard<'id, 's, RawSleepablelock, T>;
+pub type BrandedSleeplockGuard<'id, 's, T> = BrandedGuard<'id, 's, RawSleeplock, T>;
+pub type BrandedSpinlockGuard<'id, 's, T> = BrandedGuard<'id, 's, RawSpinlock, T>;
+
+pub struct BrandedRemoteLock<'id, 's, R: RawLock, U, T> {
+    lock: &'s BrandedLock<'id, R, U>,
+    data: UnsafeCell<T>,
+}
+
+pub type BrandedRemoteSleepablelock<'id, 's, U, T> =
+    BrandedRemoteLock<'id, 's, RawSleepablelock, U, T>;
+pub type BrandedRemoteSleeplock<'id, 's, U, T> = BrandedRemoteLock<'id, 's, RawSleeplock, U, T>;
+pub type BrandedRemoteSpinlock<'id, 's, U, T> = BrandedRemoteLock<'id, 's, RawSpinlock, U, T>;
+
+/* impl BrandedLock */
+impl<'id, T> BrandedSpinlock<'id, T> {
+    /// Creates a new `BrandedSpinlock` that can be used within the given closure.
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new<F: for<'new_id> FnOnce(BrandedSpinlock<'new_id, T>) -> V, V>(
+        name: &'static str,
+        data: T,
+        f: F,
+    ) -> V {
+        f(Self {
+            _marker: PhantomData,
+            lock: Spinlock::new(name, data),
+        })
+    }
+}
+
+impl<'id, R: RawLock, T> BrandedLock<'id, R, T> {
+    pub fn lock(&self) -> BrandedGuard<'id, '_, R, T> {
+        BrandedGuard {
+            _marker: PhantomData,
+            guard: self.lock.lock(),
+        }
+    }
+}
+
+impl<'id, R: RawLock, T> Deref for BrandedLock<'id, R, T> {
+    type Target = Lock<R, T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.lock
+    }
+}
+
+impl<'id, R: RawLock, T> DerefMut for BrandedLock<'id, R, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.lock
+    }
+}
+
+/* impl BrandedGuard */
+impl<'id, 's, R: RawLock, T> Deref for BrandedGuard<'id, 's, R, T> {
+    type Target = Guard<'s, R, T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.guard
+    }
+}
+
+impl<'id, 's, R: RawLock, T> DerefMut for BrandedGuard<'id, 's, R, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.guard
+    }
+}
+
+/* impl BrandedRemoteLock */
+impl<'id, 's, R: RawLock, U, T> BrandedRemoteLock<'id, 's, R, U, T> {
+    pub fn new(lock: &'s BrandedLock<'id, R, U>, data: T) -> Self {
+        Self {
+            lock,
+            data: UnsafeCell::new(data),
+        }
+    }
+
+    pub fn get_pin_mut<'a>(&self, _guard: &'a mut BrandedGuard<'id, '_, R, U>) -> Pin<&'a mut T> {
+        unsafe { Pin::new_unchecked(&mut *self.data.get()) }
+    }
+}
+
+fn test() {
+    BrandedSpinlock::new("lock1", 10, |lock1| {
+        BrandedSpinlock::new("lock2", 20, |lock2| {
+            let mut guard1 = lock1.lock();
+            let mut guard2 = lock2.lock();
+
+            let remote_lock1 = BrandedRemoteSpinlock::new(&lock1, 100);
+            let remote_lock2 = BrandedRemoteSpinlock::new(&lock2, 200);
+            assert!(*remote_lock1.get_pin_mut(&mut guard1) == 100);
+            assert!(*remote_lock2.get_pin_mut(&mut guard2) == 200);
+            // assert!(*remote_lock1.get_mut(&mut guard2) == 100); // Error! Compile fails!
+        });
+    });
 }
