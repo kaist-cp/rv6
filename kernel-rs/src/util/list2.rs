04b8d86a5a7d608a7a02c43446599224bb10c202
travis1829 <travis1829@naver.com>
2021-04-23 14:46:53 +0000

    Small fix.

--- kernel-rs/src/util/list2.rs
+++ kernel-rs/src/util/list2.rs
@@ -1,25 +1,36 @@
 //! A doubly linked list that does not owns its nodes.
 //!
+//! [`List`] safely implements a doubly linked list that does not owns its nodes.
+//! This is done by the following invariants.
+//! * `NodeRef`/`NodeMut` exists only while the `Node` is inside a `List`.
+//! * A `NodeRef` immutably borrows both the node **and the list** for its lifetime.
+//! * A `NodeMut` mutably borrows both the node **and the list** for its lifetime.
+//! * If a `Node` drops while its still inside a `List`, we panic. (This is the only runtime cost we have.)
+//!
+//! In this way, we can safely implement a list without restricting functionality
+//! (e.g. disallowing nodes from getting removed from a list once it gets inserted,
+//! or disallowing nodes from getting dropped before the list even after its already removed, etc).
+//!
+//! Also, note that in this way, we make the `List` logically the *borrow owner* of all of its nodes. That is,
+//! * You always need a `ListRef` to immutably access a `Node`.
+//! * You always need a `ListMut` to mutably access a `Node`.
+//!
+//! # Lists that does not own its nodes
+//!
 //! In Rust, a list usually owns its nodes. This is the easiest way to guarantee safety,
 //! since we can access the elements only through the list's API in this way.
 //!
 //! However, often, we need a list that does not owns its nodes.
 //! For example, the nodes may need to be scattered all around instead of being together in a single array.
 //! Or, the nodes may need to be stored not only on the heap, but also on the stack or global data.
-//! To implement such lists in a safe way, we usually needed to add extra restrictions on functionality,
-//! such as that the node can never be removed from the list once it gets inserted,
-//! or that the node must not drop before the list drops (even after the node was already popped from the list), etc.
-//! Otherwise, we needed to add a lot of runtime cost.
 //!
-//! In this module's [`List`], we use a bit different way. We used the following invariants.
-//! * `NodeRef`/`NodeMut` exists only while the `Node` is inside a `List`.
-//! * Only one `NodeMut` exists for each `List`.
-//! * To obtain a `NodeRef`, we need to immutably borrow both the node *and the list*.
-//! * To obtain a `NodeMut`, we need to mutably borrow both the node *and the list*.
-//! * If a `Node` drops while its still inside a `List`, we panic. (This is the only runtime cost we have.)
-// TODO: Add documentation
+//! This module's [`List`] implements such list without sacrificying functionality. Note that
+//! * A `Node` can be stored anywhere. (e.g. On the stack, heap, global data, etc.)
+//! * A `Node` can drop at any time if it is not inside a `List`.
+//! (i.e. A `Node` does not need to statically outlive the `List`, and conversely, the `List` does not need to statically outlive the `Node`s.)
 
 use core::marker::PhantomData;
+use core::ops::{Deref, DerefMut};
 use core::pin::Pin;
 
 use pin_project::{pin_project, pinned_drop};
@@ -27,14 +38,7 @@ use pin_project::{pin_project, pinned_drop};
 use super::list::ListEntry;
 
 /// A doubly linked list that does not own the `Node`s.
-/// * A `Node` can be stored anywhere. (e.g. On the stack, heap, global data, etc.)
-/// * A `Node` can drop at any time if it is not inside a `List`.
-/// (i.e. A `Node` does not need to statically outlive the `List`, and conversely, the `List` does not need to statically outlive the `Node`s.)
-/// * However, a `Node` must be pinned, and it must not drop while inside a `List`.
-///
-/// The list is logically the "borrow owner" of all of its inner `Node`s. That is,
-/// * If you have a `ListRef`, you can immutably dereference any inner `Node`s.
-/// * If you have a `ListMut`, you can mutably dereference any inner `Node`s.
+/// See the module documentation for details.
 #[pin_project]
 pub struct List<T> {
     #[pin]
@@ -44,13 +48,10 @@ pub struct List<T> {
 
 /// An immutable reference to a `List`.
 /// Grants immutable access to the `List` and any of its `Node`s.
-// SAFETY: No `NodeMut` exists.
 pub struct ListRef<'s, T>(&'s List<T>);
 
 /// A mutable reference to a `List`.
 /// Grants unique mutable access to the `List` and any of its `Node`s.
-// SAFETY: No `NodeRef` exists.
-//         Only one `NodeMut` exists for each `ListMut`.
 pub struct ListMut<'s, T>(Pin<&'s mut List<T>>);
 
 /// A node that can be inserted into a `List`.
@@ -80,6 +81,10 @@ pub struct NodeMut<'s, T>(Pin<&'s mut Node<T>>);
 impl<T> List<T> {
     /// Returns a new `List`.
     /// Use `List::get_ref` or `List::get_mut` to do something with the `List`.
+    ///
+    /// # Safety
+    ///
+    /// Use after initialization.
     pub unsafe fn new() -> Self {
         Self {
             head: unsafe { ListEntry::new() },
@@ -93,12 +98,13 @@ impl<T> List<T> {
     }
 
     /// Returns a `ListRef` of this `List`.
-    pub fn get_ref(&self) -> ListRef<'_, T> {
+    pub fn as_ref(&self) -> ListRef<'_, T> {
         ListRef(self)
     }
 
     /// Returns a `ListMut` of this `List`.
-    pub fn get_mut(self: Pin<&mut Self>) -> ListMut<'_, T> {
+    #[allow(clippy::wrong_self_convention)]
+    pub fn as_mut(self: Pin<&mut Self>) -> ListMut<'_, T> {
         ListMut(self)
     }
 }
@@ -110,6 +116,11 @@ impl<'s, T> ListRef<'s, T> {
     }
 
     /// Provides a `NodeRef` to the back element, or `None` if the list is empty.
+    // SAFETY: `NodeRef` does not actually borrow the `Node` here.
+    // However, this is safe since we cannot obtain a `ListMut`
+    // (and hence, a `NodeMut`) while a `NodeRef` exists.
+    // That is, we cannot mutably access/remove the `Node`.
+    // Also, the `Node` does not drop while a `NodeRef` exists.
     pub fn back(&self) -> Option<NodeRef<'_, T>> {
         if self.is_empty() {
             None
@@ -120,6 +131,11 @@ impl<'s, T> ListRef<'s, T> {
     }
 
     /// Provides a `NodeRef` to the front element, or `None` if the list is empty.
+    // SAFETY: `NodeRef` does not actually borrow the `Node` here.
+    // However, this is safe since we cannot obtain a `ListMut`
+    // (and hence, a `NodeMut`) while a `NodeRef` exists.
+    // That is, we cannot mutably access/remove the `Node`.
+    // Also, the `Node` does not drop while a `NodeRef` exists.
     pub fn front(&self) -> Option<NodeRef<'_, T>> {
         if self.is_empty() {
             None
@@ -155,8 +171,9 @@ impl<'s, T> ListMut<'s, T> {
         }
     }
 
-    // SAFETY: `NodeMut` borrows the `List`, but does not actually borrow the `Node`.
-    //         However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // SAFETY: `NodeMut` does not actually borrow the `Node` here.
+    // However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // Also, the `Node` does not drop while a `NodeMut` exists.
     pub fn back_mut(&mut self) -> Option<NodeMut<'_, T>> {
         if self.is_empty() {
             None
@@ -166,8 +183,9 @@ impl<'s, T> ListMut<'s, T> {
         }
     }
 
-    // SAFETY: `NodeMut` borrows the `List`, but does not actually borrow the `Node`.
-    //         However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // SAFETY: `NodeMut` does not actually borrow the `Node` here.
+    // However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // Also, the `Node` does not drop while a `NodeMut` exists.
     pub fn front_mut(&mut self) -> Option<NodeMut<'_, T>> {
         if self.is_empty() {
             None
@@ -177,15 +195,17 @@ impl<'s, T> ListMut<'s, T> {
         }
     }
 
-    // SAFETY: `NodeMut` borrows the `List`, but does not actually borrow the `Node`.
-    //         However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // SAFETY: `NodeMut` does not actually borrow the `Node` here.
+    // However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // Also, the `Node` does not drop while a `NodeMut` exists.
     pub fn push_back<'t>(&'t mut self, node: Pin<&'t mut Node<T>>) -> NodeMut<'t, T> {
         self.0.head.push_back(&node.list_entry);
         NodeMut(node)
     }
 
-    // SAFETY: `NodeMut` borrows the `List`, but does not actually borrow the `Node`.
-    //         However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // SAFETY: `NodeMut` does not actually borrow the `Node` here.
+    // However, this is safe since only one `NodeMut` exists for each `List` anyway.
+    // Also, the `Node` does not drop while a `NodeMut` exists.
     pub fn push_front<'t>(&'t mut self, node: Pin<&'t mut Node<T>>) -> NodeMut<'t, T> {
         self.0.head.push_front(&node.list_entry);
         NodeMut(node)
@@ -211,6 +231,11 @@ impl<T> Node<T> {
 
     // TODO: use `offset_of!` instead.
 
+    /// Returns a new `Node`.
+    ///
+    /// # Safety
+    ///
+    /// Use after initialization.
     pub unsafe fn new(data: T) -> Self {
         Self {
             list_entry: unsafe { ListEntry::new() },
@@ -222,17 +247,38 @@ impl<T> Node<T> {
         self.project().list_entry.init();
     }
 
+    /// Returns an immutable reference to the inner data if the `Node` is not inside a `List`.
+    /// Otherwise, returns `None`.
+    pub fn get(&self) -> Option<&T> {
+        if self.list_entry.is_unlinked() {
+            Some(&self.data)
+        } else {
+            None
+        }
+    }
+
+    /// Returns an immutable reference to the inner data if the `Node` is not inside a `List`.
+    /// Otherwise, returns `None`.
+    pub fn get_mut(&mut self) -> Option<&mut T> {
+        if self.list_entry.is_unlinked() {
+            Some(&mut self.data)
+        } else {
+            None
+        }
+    }
+
     /// # Safety
     ///
     /// The `Node` must already be inserted inside the list.
-    pub unsafe fn get_ref_unchecked<'s>(&'s self, _list_ref: &'s ListRef<'_, T>) -> NodeRef<'s, T> {
+    pub unsafe fn as_ref_unchecked<'s>(&'s self, _list_ref: &'s ListRef<'_, T>) -> NodeRef<'s, T> {
         NodeRef(self)
     }
 
     /// # Safety
     ///
     /// The `Node` must already be inserted inside the list.
-    pub unsafe fn get_mut_unchecked<'s>(
+    #[allow(clippy::wrong_self_convention)]
+    pub unsafe fn as_mut_unchecked<'s>(
         self: Pin<&'s mut Self>,
         _list_mut: &'s mut ListMut<'_, T>,
     ) -> NodeMut<'s, T> {
@@ -256,22 +302,30 @@ impl<T> PinnedDrop for Node<T> {
     }
 }
 
-impl<'s, T> NodeRef<'s, T> {
-    pub fn get(&self) -> &T {
+impl<T> Deref for NodeRef<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
         &self.0.data
     }
 }
 
 impl<'s, T> NodeMut<'s, T> {
-    pub fn get(&self) -> &T {
-        &self.0.data
-    }
-
-    pub fn get_mut(&mut self) -> &mut T {
-        self.0.as_mut().project().data
-    }
-
     pub fn remove(self) {
         self.0.list_entry.remove();
+    }
+}
+
+impl<T> Deref for NodeMut<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0.data
+    }
+}
+
+impl<T> DerefMut for NodeMut<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.0.as_mut().project().data
     }
 }
