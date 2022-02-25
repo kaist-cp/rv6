eafc542e93ac4d04e629eb05d5a0d74dc3c58930
junsooo <junsoolee@kaist.ac.kr>
2022-02-25 22:45:40 +0900

    fix smc_call func to return x0, x1, x2, x3

--- kernel-rs/src/arch/arm/asm.rs
+++ kernel-rs/src/arch/arm/asm.rs
@@ -135,12 +135,12 @@ pub enum SmcFunctions {
 /// Arguments must follow ARM SMC calling convention.
 #[no_mangle]
 pub unsafe fn smc_call(x0: u64, x1: u64, x2: u64, x3: u64) -> u64 {
-    let r;
+    let (r0, r1, r2, r3);
     unsafe {
         // NOTE: here use hvc for qemu without `virtualization=on`
-        asm!("hvc #0", inlateout("x0") x0 => r, in("x1") x1, in("x2") x2, in("x3") x3);
+        asm!("hvc #0", inlateout("x0") x0 => r0, inout("x1") x1 => r1, inout("x2") x2 => r2, inout("x3") x3 => r3);
     }
-    r
+    (r0, r1, r2, r3)
 }
 
 pub fn cpu_relax() {
