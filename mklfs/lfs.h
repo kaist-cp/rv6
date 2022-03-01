0b656422f9e7301a07987be3269686c738d001ba
travis1829 <travis1829@naver.com>
2022-03-02 01:03:16 +0900

    Implement mklfs.

--- mklfs/lfs.h
+++ mklfs/lfs.h
@@ -1,26 +1,52 @@
-// On-disk file system format.
+// On-disk file system format for the lfs..
 // Both the kernel and user programs use this header file.
 
 #include "kernel/types.h"
 
-#define ROOTINO  1   // root i-number
+#define ROOTINO  1  // root i-number
 #define BSIZE 1024  // block size
+#define SEGSIZE 10  // segment size in blocks
+#define FSSIZE 5000 // size of file system in blocks
+
+// assumes inum : 0 ~ NINODES - 1
+#define NINODES 200
+#define NMETA 4
+#define NSEG ((FSSIZE - NMETA) / SEGSIZE)
+#define NINODEMAP ((NINODES * sizeof(uint) + BSIZE - 1) / BSIZE)
 
 // Disk layout:
-// [ boot block | super block | log | inode blocks |
-//                                          free bit map | data blocks]
+// [ boot block | super block | checkpoint1  | checkpoint2 |
+//                                          inode map, inode blocks, and data blocks ]
 //
-// mkfs computes the super block and builds an initial file system. The
+// mklfs computes the super block and builds an initial file system. The
 // super block describes the disk layout:
 struct superblock {
   uint magic;        // Must be FSMAGIC
   uint size;         // Size of file system image (blocks)
   uint nblocks;      // Number of data blocks
-  uint ninodes;      // Number of inodes.
-  uint nlog;         // Number of log blocks
-  uint logstart;     // Block number of first log block
-  uint inodestart;   // Block number of first inode block
-  uint bmapstart;    // Block number of first free map block
+  uint nsegments;    // Number of segments
+  uint ninodes;      // Number of inodes
+  uint checkpoint1;  // Block number of first checkpoint block
+  uint checkpoint2;  // Block number of second checkpoint block
+  uint segstart;     // Block number of first segment
+};
+
+// The size of the segment usage table in bytes. Always a multiple of 4.
+#define SEGTABLESIZE ((NSEG + (sizeof(uint) * 8 - 1)) / (sizeof(uint) * 8) * 4)
+
+struct checkpoint {
+  uint imap[NINODEMAP];
+  uchar segtable[SEGTABLESIZE]; // bitmap
+  uint timestamp;
+};
+
+// Number of entries in each on-disk imap block.
+#define NENTRY (BSIZE / sizeof(uint))
+
+// A part of the imap stored in a single disk block.
+// The actual imap may be stored in more than one block.
+struct dimap {
+  uint addr[NENTRY];
 };
 
 #define FSMAGIC 0x10203040
@@ -39,17 +65,8 @@ struct dinode {
   uint addrs[NDIRECT+1];   // Data block addresses
 };
 
-// Inodes per block.
-#define IPB           (BSIZE / sizeof(struct dinode))
-
 // Block containing inode i
-#define IBLOCK(i, sb)     ((i) / IPB + sb.inodestart)
-
-// Bitmap bits per block
-#define BPB           (BSIZE*8)
-
-// Block of free map containing bit for block b
-#define BBLOCK(b, sb) ((b)/BPB + sb.bmapstart)
+#define IBLOCK(i, imp)     (imp[i])
 
 // Directory is a file containing a sequence of dirent structures.
 #define DIRSIZ 14
