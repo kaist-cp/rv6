48ec13ff7bbddeb4be2ff9e2b9458174338a1f12
Miles <miles@Miless-MacBook-Pro.local>
2023-01-10 01:55:02 +0330

    include comments

--- kernel/bio.c
+++ kernel/bio.c
@@ -1,8 +1,28 @@
+// Buffer cache.
+//
+// The buffer cache is a linked list of buf structures holding
+// cached copies of disk block contents.  Caching disk blocks
+// in memory reduces the number of disk reads and also provides
+// a synchronization point for disk blocks used by multiple processes.
+//
+// Interface:
+// * To get a buffer for a particular disk block, call bread.
+// * After changing buffer data, call bwrite to write it to disk.
+// * When done with the buffer, call brelse.
+// * Do not use the buffer after calling brelse.
+// * Only one process at a time can use a buffer,
+//     so do not keep them longer than necessary.
+
+
+
+#include "types.h"
+#include "param.h"
+#include "riscv.h"
+#include "defs.h"
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <pthread.h>
-
 #include "fs.h"
 #include "buf.h"
 #include "virtio_disk.h"
